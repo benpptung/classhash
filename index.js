5a40b54a9978c9df7d816a9b74c9c9884a932b75
benpptung <benpptung@tacol.biz>
2017-09-03 12:37:26 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+module.exports = ClassHash
+
+/**
+ * @param  {string} hash
+ * @return {function}
+ */
+function ClassHash(hash) {
+
+  /**
+   * @param  {object} classnames
+   * @return {object}
+   */
+  return function(classnames) {
+
+    let classes = {}
+    Object.keys(classnames).forEach(function(key) {
+      if (!classnames[key]) return
+      if (typeof classnames[key] !== 'string') return classes[key] = key + hash
+      classes[key] = classnames[key].split(/\s+/).filter(Boolean).map(str=> str + hash).join(' ')
+    })
+
+    return classes
+  }
+}
